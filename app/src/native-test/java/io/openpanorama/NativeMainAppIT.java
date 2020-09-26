e40705a4c21d8c2b9d9745cff7beb1bbd1a7bfaf
Sheikh Sajid <sheikh.sajid@simpanatech.com>
2020-09-26 10:53:02 +0300

    initial commit

---
+++ app/src/native-test/java/io/openpanorama/NativeMainAppIT.java
@@ -0,0 +1,10 @@
+package io.openpanorama;
+
+import io.openpanorama.MainAppTest;
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMainAppIT extends MainAppTest {
+
+    // Execute the same tests but in native mode.
+}
